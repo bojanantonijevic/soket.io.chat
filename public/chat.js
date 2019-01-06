ba18540fbcb74147ecdcd2ed30ae59671203da76
Bojan Antonijevic <bojan.antonijevic@live.com>
2019-01-05 23:01:42 -0600

    Update chat.js

--- public/chat.js
+++ public/chat.js
@@ -1,6 +1,6 @@
 //Make connection
 var PORT = process.env.PORT || 3000;
-var socket = io.connect('https://localhost:'+ PORT+');
+var socket = io.connect('https://localhost:'+ PORT);
 
 // Query DOM 
 
