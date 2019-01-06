f4424e870a2266f91de7564e6a9ac2ab01f2fe9d
Bojan Antonijevic <bojan.antonijevic@live.com>
2019-01-05 22:14:21 -0600

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var socket = require('socket.io');
 var app = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;;
 
 var server = app.listen(PORT, () => {
 	console.log(`Our app is running on port ${ PORT }`);
